7e48f20da3424c9dd228bc2ce018f706f180e51f
Omri <OmriG.N7@gmail.com>
2022-02-22 18:04:22 -0800

    Added modifier

---
+++ notifier.py
@@ -0,0 +1,8 @@
+
+
+#https://www.reddit.com/r/Python/comments/8gb88e/free_alternatives_to_twilio_for_sending_text/
+
+class Notifier:
+    def __init__(self):
+        pass
+        # implement stuff here :-)
